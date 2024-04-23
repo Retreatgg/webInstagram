0ffb77c27149d4ec3fac745f505b10deb4181792
Argen Ashimov <retreatpm@icloud.com>
2024-04-23 19:33:35 +0600

    Start labwork

---
+++ src/main/java/com/example/webinstagram/WebInstagramApplication.java
@@ -0,0 +1,13 @@
+package com.example.webinstagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebInstagramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebInstagramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webinstagram/WebInstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webinstagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebInstagramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
